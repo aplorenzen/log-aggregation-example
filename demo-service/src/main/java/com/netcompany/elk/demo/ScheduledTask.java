16a0929dbc83ddf546d18f5cf51b3f25cb15a898
Andreas Ploug Lorenzen <apl@netcompany.com>
2019-10-20 13:49:20 +0200

    Filebeat and logstash configs confirmed working with demo-service application

---
+++ demo-service/src/main/java/com/netcompany/elk/demo/ScheduledTask.java
@@ -0,0 +1,21 @@
+package com.netcompany.elk.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+public class ScheduledTask {
+
+    @Scheduled(fixedRate = 1000L)
+    public void logSomething() {
+        Map<String, String> env = System.getenv();
+        env.forEach((key, value) -> {
+            log.info("{} = {}", key, value);
+        });
+
+    }
+}
---
+++ demo-service/src/test/java/com/netcompany/elk/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcompany.elk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
